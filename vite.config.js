b863244fd4b717c54274a51d90562ac39f25629a
myreedbricks <reedbricks@gmail.com>
2022-08-22 02:58:56 +0100

    Initial client setup

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from "path";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve, join } from "path";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "#": resolve(__dirname, "src/assets"),
-      "@": resolve(__dirname, "src"),
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  plugins: {
+    viteStaticCopy: {
+      targets: {
+        src: 'assets',
+        dest: join(__dirname, 'dist')
+      }
     }
   }
 })
