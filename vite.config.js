70ee702b72d925d600085fce7227a3557c5f35d5
myreedbricks <reedbricks@gmail.com>
2022-08-22 08:55:48 +0100

    Updated NUPO Banners

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve, join } from "path";
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  plugins: {
-    viteStaticCopy: {
-      targets: {
-        src: 'assets',
-        dest: join(__dirname, 'dist')
-      }
+  resolve: {
+    alias: {
+      "#": resolve(__dirname, "src/assets"),
+      "@": resolve(__dirname, "src"),
     }
   }
 })
