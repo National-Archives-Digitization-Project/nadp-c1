838cf608a66d4ed50e81f8357a29d118cfaf9a80
myreedbricks <reedbricks@gmail.com>
2022-08-24 19:59:00 +0100

    Route issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 
 import React, { Fragment, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './components/Login'
 import Dashboard from './components/Dashboard';
 
 const App = () => {
   const [initialState, setInitialState] = useState([]);
-
   return (
     <Fragment>
       <BrowserRouter>
-        <Switch>
+        <Routes>
           <Route path="/" component={Login} />
           <Route path="/dashboard/" exact component={Dashboard} />
-        </Switch>
+        </Routes>
       </BrowserRouter>
     </Fragment>
   )
