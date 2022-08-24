5b83fd8ef8b46b3d82224651be133e00c18140b0
myreedbricks <reedbricks@gmail.com>
2022-08-24 23:40:40 +0100

    Route issue fix

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,17 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [react()],
   build: {
+    outDir: "./dist",
     sourcemap: false,
     emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      "#": resolve(__dirname, "src/assets"),
+      "@": resolve(__dirname, "src"),
+    }
   }
 })
