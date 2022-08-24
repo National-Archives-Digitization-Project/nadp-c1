6662f01c49321a197f45f76d7431ddee4ea9d9d3
myreedbricks <reedbricks@gmail.com>
2022-08-24 10:38:34 +0100

    Vite

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios';
 import Session from 'react-session-api';
-import useLocalStorage from "use-local-storage";
 import { useAppContext } from '../../context/Context';
 
 
