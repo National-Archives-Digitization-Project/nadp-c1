926918cdcf7709e38181a0ef40a836e9c9aabb77
myreedbricks <reedbricks@gmail.com>
2022-08-27 10:51:10 +0100

    Routing issues

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,11 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "https://nadpclient.vercel.app/",
   plugins: [react()],
   build: {
     outDir: resolve(__dirname, 'dist'),
     sourcemap: false,
-    emptyOutDir: false,
-    rollupOptions: {
-      input: 'src/main.jsx',
-    }
+    emptyOutDir: false
   }
 })
